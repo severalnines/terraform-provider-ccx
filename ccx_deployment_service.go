ed7c736a7da153d3c40defcc47c9414cd7ed3889
Milen Hristov <vele@Milens-MacBook-Air.local>
2021-01-07 11:16:42 +0000

    added GetClusterByID

--- ccx_deployment_service.go
+++ ccx_deployment_service.go
@@ -67,6 +67,71 @@ type DeploymentServiceResponse []struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type ClusterDetailResponse struct {
+	UUID          string `json:"uuid"`
+	DatabaseNodes []struct {
+		HostUUID         string      `json:"host_uuid"`
+		ClusterUUID      string      `json:"cluster_uuid"`
+		CloudProvider    string      `json:"cloud_provider"`
+		HostType         interface{} `json:"host_type"`
+		InstanceID       string      `json:"instance_id"`
+		InstanceType     string      `json:"instance_type"`
+		Region           string      `json:"region"`
+		PublicIP         interface{} `json:"public_ip"`
+		PrivateIP        string      `json:"private_ip"`
+		Fqdn             interface{} `json:"fqdn"`
+		Disks            string      `json:"disks"`
+		Vpc              string      `json:"vpc"`
+		Subnet           string      `json:"subnet"`
+		SecurityGroup    string      `json:"security_group"`
+		CreatedAt        time.Time   `json:"created_at"`
+		UpdatedAt        time.Time   `json:"updated_at"`
+		ServiceStartedAt time.Time   `json:"service_started_at"`
+		UniqueID         int         `json:"unique_id"`
+		LastSeenAt       time.Time   `json:"last_seen_at"`
+		AdminPort        int         `json:"admin_port"`
+		Port             int         `json:"port"`
+		ListeningPort    int         `json:"listening_port"`
+		ListeningPortRo  int         `json:"listening_port_ro"`
+		Hostname         string      `json:"hostname"`
+		HostStatus       string      `json:"host_status"`
+		Role             string      `json:"role"`
+		NodeType         string      `json:"node_type"`
+		SslEnabled       bool        `json:"ssl_enabled"`
+	} `json:"database_nodes"`
+	LoadBalancerNodes []struct {
+		HostUUID         string      `json:"host_uuid"`
+		ClusterUUID      string      `json:"cluster_uuid"`
+		CloudProvider    string      `json:"cloud_provider"`
+		HostType         interface{} `json:"host_type"`
+		InstanceID       string      `json:"instance_id"`
+		InstanceType     string      `json:"instance_type"`
+		Region           string      `json:"region"`
+		PublicIP         interface{} `json:"public_ip"`
+		PrivateIP        string      `json:"private_ip"`
+		Fqdn             interface{} `json:"fqdn"`
+		Disks            string      `json:"disks"`
+		Vpc              string      `json:"vpc"`
+		Subnet           string      `json:"subnet"`
+		SecurityGroup    string      `json:"security_group"`
+		CreatedAt        time.Time   `json:"created_at"`
+		UpdatedAt        time.Time   `json:"updated_at"`
+		ServiceStartedAt time.Time   `json:"service_started_at"`
+		UniqueID         int         `json:"unique_id"`
+		LastSeenAt       time.Time   `json:"last_seen_at"`
+		AdminPort        int         `json:"admin_port"`
+		Port             int         `json:"port"`
+		ListeningPort    int         `json:"listening_port"`
+		ListeningPortRo  int         `json:"listening_port_ro"`
+		Hostname         string      `json:"hostname"`
+		HostStatus       string      `json:"host_status"`
+		Role             string      `json:"role"`
+		NodeType         string      `json:"node_type"`
+		SslEnabled       bool        `json:"ssl_enabled"`
+	} `json:"load_balancer_nodes"`
+}
+
 type ClusterTableHeaders struct {
 	Name             string `header:"cluster name"`
 	Status           string `header:"status"`
@@ -74,6 +139,18 @@ type ClusterTableHeaders struct {
 	Databasevendor   string `header:"Vendor"`
 	Databaseversion  string `header:"Database Version"`
 	Databaseendpoint string `header:"Database Endpoint"`
+}
+
+type ClusterDetailHeaders struct {
+	Uuid      string      `header:"UUID"`
+	Hostname  string      `header:"Hostname"`
+	Vpc       string      `header:"VPC"`
+	PublicIP  interface{} `header:"Public IP"`
+	PrivateIP string      `header:"Private IP"`
+	Status    string      `header:"Status"`
+	Port      int         `header:"Port"`
+	Role      string      `header:"Role"`
+	NodeType  string      `header:"Type"`
 }
 
 func GetClusters(userId string, cookie *http.Cookie) {
@@ -90,15 +167,52 @@ func GetClusters(userId string, cookie *http.Cookie) {
 	}
 	defer res.Body.Close()
 	printer := tableprinter.New(os.Stdout)
-
 	responseBody, _ := ioutil.ReadAll(res.Body)
 	var ServiceResponse DeploymentServiceResponse
 	var table []ClusterTableHeaders
 	json.Unmarshal(responseBody, &ServiceResponse)
 	for i := range ServiceResponse {
-		table = append(table, ClusterTableHeaders{ServiceResponse[i].ClusterName,
-			ServiceResponse[i].ClusterStatus, ServiceResponse[i].UUID,
-			ServiceResponse[i].DatabaseVendor, ServiceResponse[i].DatabaseVersion, ServiceResponse[i].DatabaseEndpoint})
+		table = append(table,
+			ClusterTableHeaders{ServiceResponse[i].ClusterName,
+				ServiceResponse[i].ClusterStatus,
+				ServiceResponse[i].UUID,
+				ServiceResponse[i].DatabaseVendor,
+				ServiceResponse[i].DatabaseVersion,
+				ServiceResponse[i].DatabaseEndpoint})
+	}
+	printer.Print(table)
+}
+
+func GetClusterByID(uuid string, cookie *http.Cookie) {
+	BaseURLV1 := "https://ccx-deployment-service.s9s-dev.net/api/v1/deployment/" + uuid
+	req, _ := http.NewRequest("GET", BaseURLV1, nil)
+	req.AddCookie(cookie)
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	if res.StatusCode != 200 {
+		log.Fatal(res.Status)
+	}
+	defer res.Body.Close()
+	printer := tableprinter.New(os.Stdout)
+	responseBody, _ := ioutil.ReadAll(res.Body)
+	var ServiceResponse ClusterDetailResponse
+	var table []ClusterDetailHeaders
+	json.Unmarshal(responseBody, &ServiceResponse)
+	for i := range ServiceResponse.DatabaseNodes {
+		table = append(table,
+			ClusterDetailHeaders{ServiceResponse.UUID,
+				ServiceResponse.DatabaseNodes[i].Hostname,
+				ServiceResponse.DatabaseNodes[i].Vpc,
+				ServiceResponse.DatabaseNodes[i].PublicIP,
+				ServiceResponse.DatabaseNodes[i].PrivateIP,
+				ServiceResponse.DatabaseNodes[i].HostStatus,
+				ServiceResponse.DatabaseNodes[i].Port,
+				ServiceResponse.DatabaseNodes[i].Role,
+				ServiceResponse.DatabaseNodes[i].NodeType,
+			})
 	}
 	printer.Print(table)
 }
--- main.go
+++ main.go
@@ -4,5 +4,6 @@ func main() {
 	c := CCXAuth("milen", "password")
 	id, token := c.GetUserId()
 	GetClusters(id, token)
+	GetClusterByID("41def0d0-d447-4bf2-9990-9a5066bd8b88", token)
 
 }
