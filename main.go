2ba700fb2a5071d86d5416fc1c01d03de4f0e7ea
Milen Hristov <vele@Milens-MacBook-Air.local>
2021-01-11 10:35:25 +0000

    added terraform provider definitions

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
+import "github.com/severalnines/terraform-provider-ccx/services"
+
 func main() {
-	c := CCXAuth("milen", "password")
+	c := services.CCXAuth("milen", "password")
 	id, token := c.GetUserId()
-	GetClusters(id, token)
-	GetClusterByID("41def0d0-d447-4bf2-9990-9a5066bd8b88", token)
+	services.GetClusters(id, token)
+	services.GetClusterByID("41def0d0-d447-4bf2-9990-9a5066bd8b88", token)
+	services.CreateCluster(
+		id, "spaceforce", "mariadb", "aws", "eu-west-2",
+		"mariadb", "t3.medium", 300, "milen", "dd0953", "0.0.0.0/0", token,
+	)
 
 }
