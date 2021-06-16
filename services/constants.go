023dfeb6f4d76ebe5f1564a85d18014219359121
vele <m.hristov@executie.org>
2021-06-16 14:30:29 +0100

    Fix up for staging , dev and prod environments.

---
+++ services/constants.go
@@ -0,0 +1,16 @@
+package services
+
+const (
+	VpcServiceUrlDev         = "https://ccx.s9s-dev.net/api/vpc/api/v2/vpc"
+	VpcServiceUrlTest        = "https://ccx.s9s-test.net/api/vpc/api/v2/vpc"
+	VpcServiceUrlProd        = "https://ccx.s9s.io/api/vpc/api/v2/vpc"
+	ProvServiceUrlDev        = "https://ccx.s9s-dev.net/api/prov/api/v2/cluster"
+	ProvServiceUrlTest       = "https://ccx.s9s-test.net/api/prov/api/v2/cluster"
+	ProvServiceUrlProd       = "https://ccx.s9s.io/api/prov/api/v2/cluster"
+	DeploymentServiceUrlDev  = "https://ccx.s9s-dev.net/api/deployment/v1/deployment"
+	DeploymentServiceUrlTest = "https://ccx.s9s-test.net/api/deployment/v1/deployment"
+	DeploymentServiceUrlProd = "https://ccx.s9s.io/api/deployment/v1/deployment"
+	AuthServiceUrlDev        = "https://ccx.s9s-dev.net/api/auth"
+	AuthServiceUrlTest       = "https://ccx.s9s-test.net/api/auth"
+	AuthServiceUrlProd       = "https://ccx.s9s.io/api/auth"
+)
